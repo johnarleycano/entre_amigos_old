e49d3ec76cf834272b62b5570b14e7cb14e2caef
rpoanderson22 <rpoanderson22@gmail.com>
2017-12-26 12:30:52 -0500

    Agregar cuadro préstamos

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/proyectos/entre_amigos/';
+$config['base_url']	= 'http://localhost/entre_amigos/';
 
 /*
 |--------------------------------------------------------------------------
